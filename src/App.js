b8d7cd44dd01689d8ed756e11722cfcb18c81465
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-07-21 17:24:13 -0700

    Experiment with using and updating local state in App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Gabe'
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi {this.state.name}</p>
+          <button onClick={() => {
+            this.setState({name: 'Gabriel'});
+          }}>Change Name</button>
         </header>
       </div>
     );
